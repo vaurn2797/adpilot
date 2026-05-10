8b3aed51af5740314996cdb3b123946de75aded2
Varun <varun.seo2797@gmail.com>
2026-05-10 20:46:48 +0530

    simple campaigns

--- brain.py
+++ brain.py
@@ -15,7 +15,7 @@ def get_all_accounts():
 
 def get_campaigns(account_id):
     clean_id = str(account_id).replace('act_', '')
-    url = f"https://graph.facebook.com/v21.0/act_{clean_id}/campaigns?fields=name,status,objective,daily_budget,insights.date_preset(last_30d){{spend,impressions,clicks,ctr,cpc,cpm,reach,frequency,actions,cost_per_action_type,landing_page_views,cost_per_landing_page_view}}&access_token={META_ACCESS_TOKEN}"
+    url = f"https://graph.facebook.com/v21.0/act_{clean_id}/campaigns?fields=name,status,objective,daily_budget&access_token={META_ACCESS_TOKEN}"
     r = requests.get(url)
     return r.json().get('data', [])
 
